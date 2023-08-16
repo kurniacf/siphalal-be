c992872eef0ddc60b907ec8a920e848823e326c8
AfifArdhiansyah <afifardhiansyah@gmail.com>
2023-08-16 15:19:24 +0700

    feat: implement DDD in Nest

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm.config';
-import { UsersModule } from './users/users.module';
-
-@Module({
-  imports: [ TypeOrmModule.forRoot(typeOrmConfig), UsersModule],
-})
-export class AppModule {}
