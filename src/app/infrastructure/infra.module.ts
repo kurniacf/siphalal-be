d4a286f3809a5ac2a4cbc3f45a737dd4b3aaaa34
AfifArdhiansyah <afifardhiansyah@gmail.com>
2023-08-23 23:23:13 +0700

    fix: add all schemas on infra module

--- src/app/infrastructure/infra.module.ts
+++ src/app/infrastructure/infra.module.ts
@@ -3,12 +3,40 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { UserRepository } from "./repository/user.repository";
 import { CoreModule } from "@app/core/core.module";
 import { UserSchema } from "./schemas/user.schema";
+import { UserGamificationSchema } from "@app/infrastructure/schemas/userGamification.schema";
+import { MissionSchema } from "@app/infrastructure/schemas/mission.schema";
+import { MissionTemplateSchema } from "@app/infrastructure/schemas/missionTemplate.schema";
+import { BadgeSchema } from "@app/infrastructure/schemas/badge.schema";
+import { DokumenSertifikasiHalalSchema } from "@app/infrastructure/schemas/dokumenSertifikasiHalal.schema";
+import { FeedbackUMKMSchema } from "@app/infrastructure/schemas/feedbackUMKM.schema";
+import { KiosUMKMSchema } from "@app/infrastructure/schemas/kiosUMKM.schema";
+import { OrnamentSchema } from "@app/infrastructure/schemas/ornament.schema";   
+import { PermintaanVerifikasiSchema } from "@app/infrastructure/schemas/permintaanVerifikasi.schema";
+import { ProdukUMKMSchema } from "@app/infrastructure/schemas/produkUMKM.schema";
+import { SedekahSchema } from "@app/infrastructure/schemas/sedekah.schema";
+import { StatusSertifikasiSchema } from "@app/infrastructure/schemas/statusSertifikasi.schema";
+import { SurgaKulinerSchema } from "@app/infrastructure/schemas/surgaKuliner.schema";
+import { UMKMSchema } from "@app/infrastructure/schemas/umkm.schema";
 
 @Module({
     imports: [
         CoreModule,
         MongooseModule.forFeature([
-            { name: "User", schema: UserSchema }
+            { name: "User", schema: UserSchema },
+            { name: "UserGamification", schema: UserGamificationSchema },
+            { name: "Mission", schema: MissionSchema },
+            { name: "MissionTemplate", schema: MissionTemplateSchema },
+            { name: "Badge", schema: BadgeSchema },
+            { name: "DokumenSertifikasiHalal", schema: DokumenSertifikasiHalalSchema },
+            { name: "FeedbackUMKM", schema: FeedbackUMKMSchema },
+            { name: "KiosUMKM", schema: KiosUMKMSchema },
+            { name: "Ornament", schema: OrnamentSchema },
+            { name: "PermintaanVerifikasi", schema: PermintaanVerifikasiSchema },
+            { name: "ProdukUMKM", schema: ProdukUMKMSchema },
+            { name: "Sedekah", schema: SedekahSchema },
+            { name: "StatusSertifikasi", schema: StatusSertifikasiSchema },
+            { name: "SurgaKuliner", schema: SurgaKulinerSchema },
+            { name: "UMKM", schema: UMKMSchema }
         ])
     ],
     providers: [
--- src/server/main.ts
+++ src/server/main.ts
@@ -6,10 +6,6 @@ import { load } from 'ts-dotenv';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true });
     app.useGlobalPipes(new ValidationPipe());
-    app.use("/", (req, res, next) => {
-        console.log("This is siphalal service");
-        return res.send("This is siphalal service");
-    });
     const env = load({
         PORT: String,
     });
